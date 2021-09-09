e03633edb63860710c49cb4dd7c2445a6e6db7f3
TomMakesThings <61354833+TomMakesThings@users.noreply.github.com>
2021-09-09 16:39:15 +0100

    Update VigenereCracker.java

--- VigenereCracker.java
+++ VigenereCracker.java
@@ -1,31 +1,72 @@
-/* Vigenere Cracker */
+/* Vigenere Cracker
+ * 
+ * Enter two ciphertexts encoded with the same key using a Vigenere cipher and a text 
+ * file containing a word list of possible solutions. The key (or multiple candidates) 
+ * and decrypted plaintexts are then printed.
+ * 
+ * For more detail see: https://github.com/TomMakesThings/Vigenere-Cracker
+ * 
+ *  */
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Scanner;
 
 public class VigenereCracker {
 	private String ciphertext1 = "";
 	private String ciphertext2 = "";
+	private String plaintext1 = "";
+	private String plaintext2 = "";
 	private String key = "";
+	private Integer size = 0;
 	private ArrayList<String> words = new ArrayList<String>();
 	private ArrayList<String> keys1 = new ArrayList<String>();
 	private ArrayList<String> keys2 = new ArrayList<String>();
+	private ArrayList<String> candidateKeys = new ArrayList<String>();
 	
 	/* Takes two ciphertexts and a file path */
-	public VigenereCracker(String c1, String c2, String filePath) {
+	public VigenereCracker(String c1, String c2, String filePath) throws Exception {
 		super();
-		this.ciphertext1 = c1;
-		this.ciphertext2 = c2;
+		
+		// Read the ciphertext, remove bad characters and convert to uppercase
+		this.ciphertext1 = this.filterText(c1);
+		this.ciphertext2 = this.filterText(c2);
+		// Set size as the length of the shortest ciphertext
+		this.size = Math.min(ciphertext1.length(), ciphertext2.length());
+		
+		System.out.println("Ciphertext 1: " + this.ciphertext1);
+		System.out.println("Ciphertext 2: " + this.ciphertext2);
+		
+		if (this.size > 0) {
+			System.out.println("Length of shortest ciphertext: " + this.size);
+		}
+		else {
+			throw new IllegalArgumentException("Invalid ciphertext");
+		}
+		
 		// Reads words from file
 		this.setWords(filePath);
 		// Finds possible key combinations for ciphertext
-		this.findPossibleKeys(1);
-		this.findPossibleKeys(2);
+		this.findPossibleKeys(this.ciphertext1);
+		this.findPossibleKeys(this.ciphertext2);
 		// Find matches in key sets
 		this.findMatchingKeys();
+		// Decrypt the ciphertext
+		this.decryptCiphertext();
+	}
+	
+	/* Remove non-alphabetic characters from text */
+	private String filterText(String text) {
+		text = text.replaceAll("[0-9]","");
+		String[] parts = text.split("\\W+");
+        String result = new String();
+        for(int i = 0; i < parts.length; i++){
+           result = result + parts[i];
+        }
+        return result.toUpperCase();
 	}
 	
 	/* Takes a file path to set the list of words */
@@ -35,8 +76,14 @@ public class VigenereCracker {
 	        File file = new File(filePath);
 	        Scanner scanner = new Scanner(file);  
 	        while (scanner.hasNextLine()) {
+	          // Read each word from the file
 	          String data = scanner.nextLine();
-	          words.add(data);
+	          // Remove all non-alphabetic characters
+	          String word = this.filterText(data);
+	          // Add the word if it is long enough
+	          if (word.length() >= this.size) {
+	        	 words.add(word);
+	          }
 	        }
 	        scanner.close();
 	      } catch (FileNotFoundException e) {
@@ -44,10 +91,6 @@ public class VigenereCracker {
 	        e.printStackTrace();
 	      }
 	    if (words.size() > 0) {
-	    	// Removes dot from end of list
-	    	if (words.get(words.size()-1).length() != 10) {
-	    		words.remove(words.size() - 1);
-	    	}
 	    	this.words = words;
 	    	System.out.println("Added list of words");
 	    }
@@ -56,30 +99,23 @@ public class VigenereCracker {
 	    }
 	}
 	
-	private void findPossibleKeys(int number) {
+	/* Compare the ciphertext to each word and record the difference as a potential key */
+	private void findPossibleKeys(String ciphertext) {
 		Iterator<String> i = this.words.iterator();
 		ArrayList<String> keys = new ArrayList<String>();
-		String ciphertext = "";
-		
-		if (number == 1) {
-			ciphertext = this.ciphertext1;
-		}
-		else {
-			ciphertext = this.ciphertext2;
-		}
-		
+
 		// Iterates through words in word list
 		while (i.hasNext()) {
 			String word = i.next();
 			StringBuffer key = new StringBuffer();
-			// Iterates through 10 letters in word
-			for (int j = 0; j < 10; j++) {
+			// Iterates through first n letters in word based on length of shortest ciphertext
+			for (int j = 0; j < this.size; j++) {
 				// Difference between characters at same position
 				int difference = ciphertext.charAt(j) - word.charAt(j);
 				if (difference < 0) {
 					difference += 26;
 				}
-				// Converts difference to letter
+				// Converts difference to a letter
 				char letter = (char) (difference + 65);
 				key.append(letter);
 			}
@@ -88,7 +124,7 @@ public class VigenereCracker {
 		}
 		// Checks that keys added
 		if (keys.size() > 0) {
-			if (number == 1) {
+			if (ciphertext.contentEquals(this.ciphertext1)) {
 				// Updates list of possible keys
 				this.keys1 = keys;
 				System.out.println("Created list of possible keys for ciphertext 1");
@@ -103,32 +139,120 @@ public class VigenereCracker {
 		}
 	}
 	
+	/* Find if the ciphertexts share a candidate key */
 	private void findMatchingKeys() {
-		Iterator<String> i = this.keys1.iterator();
-		ArrayList<String> matchingKeys = new ArrayList<String>();
+		// Find keys present for both ciphertexts
+		ArrayList<String> matchingKeys = new ArrayList<>(this.keys1);
+		matchingKeys.retainAll(this.keys2);
+		// Remove any duplicated values
+		ArrayList<String> candidateKeys = new ArrayList<String>(new HashSet<String>(matchingKeys));
 		
-		while (i.hasNext()) {
-			String key1 = i.next();
-			
-			Iterator<String> j = this.keys2.iterator();
-			while (j.hasNext()) {
-				String key2 = j.next();
-				if (key1.equals(key2)) {
-					matchingKeys.add(key1);
-				}
+		if (candidateKeys.size() > 0) {
+			if (candidateKeys.size() > 1) {
+				System.out.println("\nMultiple matches for key found: " + candidateKeys);
 			}
-		}
-		
-		if (matchingKeys.size() > 0) {
-			this.key = matchingKeys.get(0);
-			System.out.println("Key found: " + this.key);
+			else {
+				System.out.println("Key found: " + candidateKeys.get(0));
+			}
+			this.candidateKeys = candidateKeys;
 		}
 		else {
 			System.out.println("No matching keys");
 		}
 	}
 	
-	public static void main(String[] args) {
+	private void decryptCiphertext() throws Exception {
+		if (this.candidateKeys.size() > 0) {
+			String ciphertext = "";
+			StringBuffer plaintextBuffer;
+			
+			for (int key_idx = 0; key_idx < this.candidateKeys.size(); key_idx++) {
+				
+				key = this.candidateKeys.get(key_idx);
+				
+				if (this.candidateKeys.size() > 1) {
+					System.out.println("\nDecryption for key " + (key_idx + 1) + ": " + key);
+				}
+				
+				// Set the ciphertext to decrypt
+				for (int i = 0; i < 2; i++) {
+					plaintextBuffer = new StringBuffer();
+					
+					if (i == 0) {
+						ciphertext = this.ciphertext1;
+					}
+					else {
+						ciphertext = this.ciphertext2;
+					}
+					
+					for (int j = 0; j < this.size; j++) {
+						// Difference between characters at same position
+						int difference = ciphertext.charAt(j) - key.charAt(j);
+						if (difference < 0) {
+							difference += 26;
+						}
+						// Converts difference to a letter
+						char letter = (char) (difference + 65);
+						plaintextBuffer.append(letter);
+					}
+					
+					// Extract the string from the string buffer
+					String plaintext = plaintextBuffer.toString();
+					
+					ArrayList<String> possibleMatches = new ArrayList<String>();
+					
+					// Find whole matching words in given word list
+					for (int k = 0; k < this.words.size(); k++) {
+						String word = this.words.get(k);
+						// Check if plaintext is a substring of a word
+						if (word.startsWith(plaintext, 0)) {
+							possibleMatches.add(word);
+						}
+					}
+					
+					String plaintextPrint = plaintext;
+					
+					// Add question marks to the plaintext if the key is shorter than the ciphertext
+					if (this.ciphertext1.length() != this.ciphertext2.length()) {
+						int sizeDifference = ciphertext.length() - this.size;
+				        StringBuilder builder = new StringBuilder(sizeDifference);
+				        
+				        // Add ? for each missing character, e.g. CIPH??
+				        for(int l = 0; l < sizeDifference; l++){
+				        	builder.append("?");
+				        }
+				        
+				        plaintextPrint = plaintext + builder.toString();
+					}
+					
+					System.out.println("Plaintext " + (i + 1) + ": " + plaintextPrint);
+					
+					// Check if the plaintext is a substring of a word in the text file and print if so
+					if (possibleMatches.size() == 1 && !possibleMatches.get(0).contentEquals(plaintext)) {
+						plaintext = possibleMatches.get(0);
+						System.out.println("Likely match: " + plaintext);
+					}
+					// Otherwise print the closest matching words, so long the plaintext is not included
+					else if (possibleMatches.size() > 1 && !possibleMatches.contains(plaintext)) {
+						System.out.println("Closest matches: " + possibleMatches);
+					}
+					
+					// Set the plaintext that was decrypted using the key
+					if (i == 0) {
+						this.plaintext1 = plaintext;
+					}
+					else {
+						this.plaintext2 = plaintext;
+					}
+				}
+			}
+		}
+		else {
+			throw new Exception("Cannot decrypt ciphertext as key not found");
+		}
+	}
+	
+	public static void main(String[] args) throws Exception {
 		VigenereCracker cracker = new VigenereCracker("AFZAYMSFCU", "HNVXJKMKIG", "10letterwordslist.txt");
 	}
 }
